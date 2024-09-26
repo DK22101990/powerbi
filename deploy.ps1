38d3a3dcb46fc606c392d677e1edc19dfb6f115e
DK22101990 <dwarkeshnarvekar@gmail.com>
2024-09-26 16:26:17 +0530

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -2,14 +2,20 @@ param(
     [string]$ReportPath,
     [string]$ReportServerUri = "http://192.168.1.5/Reports",
     [string]$DestinationFolder = "/powerbifiles",
-    [string]$Overwrite = $true
+    [string]$Username,
+    [string]$Password,
+    [bool]$Overwrite = $true
 )
 
 # Load the required module
 Import-Module ReportingServicesTools
 
+# Create a PSCredential object
+$SecurePassword = ConvertTo-SecureString $Password -AsPlainText -Force
+$Credential = New-Object System.Management.Automation.PSCredential($Username, $SecurePassword)
+
 # Publish the report
-Publish-RsReport -ReportServerUri $ReportServerUri -ReportPath $ReportPath -DestinationFolder $DestinationFolder -Overwrite $Overwrite
+Publish-RsReport -ReportServerUri $ReportServerUri -ReportPath $ReportPath -DestinationFolder $DestinationFolder -Credential $Credential -Overwrite $Overwrite
 
 if ($?) {
     Write-Host "Report deployed successfully."
